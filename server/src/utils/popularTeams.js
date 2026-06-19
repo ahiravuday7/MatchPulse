e655439959916ad82b1330f05e416e35405e956c
dipakahirav <dipaksahirav@gmail.com>
2026-06-19 17:23:56 +0530

    added more teams in app

--- server/src/utils/popularTeams.js
+++ server/src/utils/popularTeams.js
@@ -1,74 +1,37 @@
 export const POPULAR_PLAYER_INDEX_TEAMS = [
-  {
-    id: 42,
-    name: "Arsenal",
-  },
-  {
-    id: 50,
-    name: "Manchester City",
-  },
-  {
-    id: 40,
-    name: "Liverpool",
-  },
-  {
-    id: 49,
-    name: "Chelsea",
-  },
-  {
-    id: 33,
-    name: "Manchester United",
-  },
-  {
-    id: 47,
-    name: "Tottenham",
-  },
-  {
-    id: 529,
-    name: "Barcelona",
-  },
-  {
-    id: 541,
-    name: "Real Madrid",
-  },
-  {
-    id: 530,
-    name: "Atletico Madrid",
-  },
-  {
-    id: 157,
-    name: "Bayern Munich",
-  },
-  {
-    id: 165,
-    name: "Borussia Dortmund",
-  },
-  {
-    id: 85,
-    name: "Paris Saint-Germain",
-  },
-  {
-    id: 505,
-    name: "Inter",
-  },
-  {
-    id: 489,
-    name: "AC Milan",
-  },
-  {
-    id: 496,
-    name: "Juventus",
-  },
-  {
-    id: 492,
-    name: "Napoli",
-  },
-  {
-    id: 2939,
-    name: "Al Nassr",
-  },
-  {
-    id: 9568,
-    name: "Inter Miami",
-  },
+  { id: 42, name: "Arsenal" },
+  { id: 50, name: "Manchester City" },
+  { id: 40, name: "Liverpool" },
+  { id: 49, name: "Chelsea" },
+  { id: 33, name: "Manchester United" },
+  { id: 47, name: "Tottenham" },
+
+  { id: 66, name: "Aston Villa" },
+
+  { id: 529, name: "Barcelona" },
+  { id: 541, name: "Real Madrid" },
+  { id: 530, name: "Atletico Madrid" },
+
+  { id: 533, name: "Villarreal" },
+  { id: 543, name: "Real Betis" },
+
+  { id: 157, name: "Bayern Munich" },
+  { id: 165, name: "Borussia Dortmund" },
+
+  { id: 173, name: "RB Leipzig" },
+  { id: 172, name: "VfB Stuttgart" },
+  { id: 167, name: "Hoffenheim" },
+
+  { id: 85, name: "Paris Saint-Germain" },
+
+  { id: 505, name: "Inter" },
+  { id: 489, name: "AC Milan" },
+  { id: 496, name: "Juventus" },
+  { id: 492, name: "Napoli" },
+
+  { id: 499, name: "Atalanta" },
+  { id: 497, name: "Roma" },
+
+  { id: 2939, name: "Al Nassr" },
+  { id: 9568, name: "Inter Miami" },
 ];
